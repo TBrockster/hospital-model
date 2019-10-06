c68326eae07fea557a49e84df64badab0f0f2ea9
TBrockster <tobybrock93@gmail.com>
2019-10-06 12:18:01 +0100

    pylint refactor of model file.

--- teamModel/models.py
+++ teamModel/models.py
@@ -3,13 +3,13 @@ from django.db import models
 # Create your models here.
 
 class TeamMember():
-  def __init__(self, firstName, lastName, type, specialities, biography):
-    self.firstName = firstName
-    self.lastName = lastName
-    self.type = type
-    self.specialities = specialities
-    self.biography = biography
-    self.onLeave = False
+    def __init__(self, first_name, last_name, type, specialities, biography):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.type = type
+        self.specialities = specialities
+        self.biography = biography
+        self.on_leave = False
 
-  def toggleOnLeave(self):
-    self.onLeave = (True, False)[self.onLeave]
+    def toggle_on_leave(self):
+        self.on_leave = (True, False)[self.on_leave]
--- teamModel/tests/team_member_name_test.py
+++ teamModel/tests/team_member_name_test.py
@@ -1,30 +1,30 @@
 from teamModel.models import TeamMember
 
-genericTeamMember = TeamMember(firstName='John', 
-                               lastName='Doe',
+genericTeamMember = TeamMember(first_name='John', 
+                               last_name='Doe',
                                type='Surgeon',
                                specialities = ['Renal', 'Paediatrics'],
                                biography='Lorem Ipsum')
 
 def test_TeamMemberHasFirstName():
-  assert genericTeamMember.firstName == 'John'
+  assert genericTeamMember.first_name == 'John'
 
 def test_TeamMemberHasLastName():
-  assert genericTeamMember.lastName == 'Doe'
+  assert genericTeamMember.last_name == 'Doe'
 
 def test_TeamMemberHasType():
   assert genericTeamMember.type == 'Surgeon'
 
 def test_TeamMemberHasOnLeave():
-  assert genericTeamMember.onLeave == False
+  assert genericTeamMember.on_leave == False
 
 def test_TeamMemberCanToggleOnLeave():
-  genericTeamMember.toggleOnLeave()
-  assert genericTeamMember.onLeave == True
+  genericTeamMember.toggle_on_leave()
+  assert genericTeamMember.on_leave == True
 
 def test_TeamMemberCanToggleOnLeaveBack():
-  genericTeamMember.toggleOnLeave()
-  assert genericTeamMember.onLeave == False
+  genericTeamMember.toggle_on_leave()
+  assert genericTeamMember.on_leave == False
 
 def test_TeamMemberHasSpecialities():
   assert genericTeamMember.specialities == ['Renal', 'Paediatrics']
