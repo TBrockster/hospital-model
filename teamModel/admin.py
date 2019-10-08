9bb1f5d1ff8f92c1978f2913a04e856305fae48b
TBrockster <tobybrock93@gmail.com>
2019-10-08 14:30:28 +0100

    fixed team member migration

--- teamModel/admin.py
+++ teamModel/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import TeamMember, Team
 
 # Register your models here.
+
+admin.site.register(TeamMember)
+admin.site.register(Team)
--- teamModel/models.py
+++ teamModel/models.py
@@ -3,13 +3,16 @@ from django.db import models
 # Create your models here.
 
 class TeamMember(models.Model):
-    profilePicture = models.CharField(max_length=255)
+    profilePicture = models.URLField()
     firstName = models.CharField(max_length=255)
     lastName = models.CharField(max_length=255)
     type = models.CharField(max_length=255)
-    onLeave = models.BooleanField
+    onLeave = models.BooleanField()
     specialities = models.CharField(max_length=255)
-    biography = models.TextField
+    biography = models.TextField()
+
+    def __str__(self):
+        return self.firstName
 
 class Team(models.Model):
     teamMembers = models.ManyToManyField(TeamMember)
