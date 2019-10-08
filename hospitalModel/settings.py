21d6a733b0021b70cce425640ac765882b95ef0b
TBrockster <tobybrock93@gmail.com>
2019-10-08 14:03:16 +0100

    made migration and migrated

--- hospitalModel/settings.py
+++ hospitalModel/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'teamModel.models.TeamMember',
+    'teamModel.apps.TeamModelConfig',
 ]
 
 MIDDLEWARE = [
--- teamModel/models.py
+++ teamModel/models.py
@@ -3,15 +3,10 @@ from django.db import models
 # Create your models here.
 
 class TeamMember(models.Model):
-    TYPES = (
-        ('Surgeon'),
-        ('Nurse'),
-        ('Admin Assistant'),
-    )
     profilePicture = models.CharField(max_length=255)
     firstName = models.CharField(max_length=255)
     lastName = models.CharField(max_length=255)
-    type = models.CharField(max_length=255, choices=TYPES)
+    type = models.CharField(max_length=255)
     onLeave = models.BooleanField
     specialities = models.CharField(max_length=255)
     biography = models.TextField
