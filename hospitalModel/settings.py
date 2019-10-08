0bec904b900162b5a7d0d8f75036d07ed80a02e4
TBrockster <tobybrock93@gmail.com>
2019-10-08 13:42:23 +0100

    made dbmodels

--- hospitalModel/settings.py
+++ hospitalModel/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'teamModel.apps.TeamModelConfig',
+    'teamModel.models.TeamMember',
 ]
 
 MIDDLEWARE = [
--- teamModel/models.py
+++ teamModel/models.py
@@ -2,3 +2,19 @@ from django.db import models
 
 # Create your models here.
 
+class TeamMember(models.Model):
+    TYPES = (
+        ('Surgeon'),
+        ('Nurse'),
+        ('Admin Assistant'),
+    )
+    profilePicture = models.CharField(max_length=255)
+    firstName = models.CharField(max_length=255)
+    lastName = models.CharField(max_length=255)
+    type = models.CharField(max_length=255, choices=TYPES)
+    onLeave = models.BooleanField
+    specialities = models.CharField(max_length=255)
+    biography = models.TextField
+
+class Team(models.Model):
+    teamMembers = models.ManyToManyField(TeamMember)
